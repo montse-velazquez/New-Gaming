daa83c701b78ac212c47b02d883196bb0db6749c
montse-velazquez <12337@coderacademy.edu.au>
2023-03-21 00:54:10 +1100

    Create CLI commnads

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema, fields
 from flask import Flask
+from flask.cli import with_appcontext
 
 app = Flask(__name__)
 db = SQLAlchemy()
@@ -19,6 +20,27 @@ class NameSchema(Schema):
     username = fields.Str()
     first_name = fields.Str()
     last_name = fields.Str()
+
+app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Table created')
+
+@app.cli.command("db_seed")
+@with_appcontext
+def seed_command():
+    user1 = Name(username='John98', first_name="John", last_name="DB")
+
+    db.session.add_all([user1, ])
+    db.session.commit()
+    print('Data seeded successfully.')
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print("Table dropped")
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
