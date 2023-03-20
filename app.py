30f36a98f0b61f63db5a7bbe6417d097f4d310cf
montse-velazquez <12337@coderacademy.edu.au>
2023-03-21 00:49:31 +1100

    Create variables and connection to the database

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow import Schema
 from flask import Flask
+
+app = Flask(__name__)
+db = SQLAlchemy()
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://montsevelazquez:myPassword@localhost:5432/gaming"
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
