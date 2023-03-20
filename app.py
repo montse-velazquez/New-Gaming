94b223f6f616d5dae4a1217e9863c84b79567f77
montse-velazquez <12337@coderacademy.edu.au>
2023-03-21 00:50:42 +1100

    Create NAME Model

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ db = SQLAlchemy()
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://montsevelazquez:myPassword@localhost:5432/gaming"
 db.init_app(app)
 
+class Name(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    first_name = db.Column(db.String(25))
+    last_name = db.Column(db.String(25))
+
 if __name__ == '__main__':
     app.run(debug=True)
+
